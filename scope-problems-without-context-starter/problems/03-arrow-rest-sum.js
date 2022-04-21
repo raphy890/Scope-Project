51d921611f92242dda996afcd77c182a7547c43d
Raphael <rapahel.yohannes@gmail.com>
2022-04-21 15:29:30 -0500

    completed prob 1-3

--- scope-problems-without-context-starter/problems/03-arrow-rest-sum.js
+++ scope-problems-without-context-starter/problems/03-arrow-rest-sum.js
@@ -1,6 +1,8 @@
 /***********************************************************************
 Write a function named `arrowRestSum` that accepts all incoming parameters
-and sums them. Assign the below function to a variable using the const keyword.
+and sums them.
+
+Assign the below function to a variable using the const keyword.
 
 **Hint**: Use rest parameter syntax!
 
@@ -11,6 +13,18 @@ arrowRestSum(0); // => 0
 ***********************************************************************/
 
 // your code here!
+const arrowRestSum = (...nums) => {
+  let sum = 0
+  for (let i = 0; i < nums.length ; i++){
+    let num = nums[i]
+    sum += num
+  }
+  return sum
+}
+
+arrowRestSum(3,5,6); // => 14
+arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 14
+arrowRestSum(0); // => 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 

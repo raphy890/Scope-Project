204d89f5f05cbdb8f90c20a8c1e964eddd041d52
Mariesa <mariesa.oconnell@gmail.com>
2022-04-21 16:46:06 -0400

    problem 05 solved

--- scope-problems-without-context-starter/problems/05-arrow-mirror-array.js
+++ scope-problems-without-context-starter/problems/05-arrow-mirror-array.js
@@ -1,17 +1,28 @@
 /***********************************************************************
-Write a function `arrowMirrorArray(array)` that takes in an array as an
-argument and returns a new array "mirrored" as shown in the examples.
+Write a function `arrowMirrorArray(array)`
+that takes in an array as an argument and returns a new array "mirrored" as shown in the examples.
 
 Write this function using an arrow function!
 
 Examples:
-
-arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
-arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
-// your code here
+const arrowMirrorArray = arr => {
+  let newArr = [];
 
+  for (let i = 0; i < arr.length; i++){
+    let el = arr[i];
+    newArr.push(el)
+  }
+  for (let i = arr.length-1; i >= 0; i--){
+    let el = arr[i]
+    newArr.push(el)
+  }
+  return newArr;
+}
+
+console.log(arrowMirrorArray([1,2,3])); // => [ 1, 2, 3, 3, 2, 1 ]
+console.log(arrowMirrorArray(['a', 'b', 'c', 'd'])) ; // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
